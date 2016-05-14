12e553fc3288939305e78e1f523eb81bfaf013bc
Thiago Parizi <thiagoparizi@gmail.com>
2016-05-14 19:37:08 +0200

    Smooth operator

--- interact.js
+++ interact.js
@@ -5,4 +5,16 @@ $(document).ready(function () {
 		});
 	$("i", this).toggleClass("fa-bars fa-times");
 	});
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({	
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
 });
