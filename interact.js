e42f70f13ff121f460de37619975d3f2d1d66b02
Thiago Parizi <thiagoparizi@gmail.com>
2016-05-15 17:27:42 +0200

    All in one

--- interact.js
+++ interact.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
 	    var $target = $(target);
 
 	    $('html, body').stop().animate({	
-	        'scrollTop': $target.offset().top - 400,
+	        'scrollTop': $target.offset().top - 300,
 	    }, 900, 'swing', function () {
 	        window.location.hash = target;
 	    });
