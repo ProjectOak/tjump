f4b195693034d29e85e16348f82e7a5b62662d4c
Thiago Parizi <thiagoparizi@gmail.com>
2016-05-15 14:23:02 +0200

    CSS & JS Improvements
    Improved:
    
    • Autoscroll
    • Nav closes on link click
    • Section spacing on mobile

--- interact.js
+++ interact.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
+
 	$( "#nav-show" ).hide();
-	$( "#show" ).click(function() {
-		$( "#nav-show" ).slideToggle(200, function() {
-		});
-	$("i", this).toggleClass("fa-bars fa-times");
+	
+	$( "#show, .nav-item" ).click(function () {
+		$( "#nav-show" ).slideToggle(200, 'linear');
+		$("#nav i").toggleClass("fa-bars fa-times");
 	});
+
 	$('a[href^="#"]').on('click',function (e) {
 	    e.preventDefault();
 
@@ -12,7 +14,7 @@ $(document).ready(function () {
 	    var $target = $(target);
 
 	    $('html, body').stop().animate({	
-	        'scrollTop': $target.offset().top
+	        'scrollTop': $target.offset().top - 400,
 	    }, 900, 'swing', function () {
 	        window.location.hash = target;
 	    });
